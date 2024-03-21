c30509a786f1a849bc01b274ff4cbb0950773fb1
Drybones2048 <93012498+Drybones2048@users.noreply.github.com>
2024-03-21 16:20:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 import yfinance as yf
+import matplotlib.pyplot as plt #needed for pie chart
+import numpy as np #needed for pie chart
+
+def isfloat(string): #a function that will check if a string is a float, with a boolean return
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def getHistory(ticker_symbol, period): #created a method for getting the history of a stock for a period of time
+    stock = yf.Ticker(ticker_symbol) #creates a ticker in the method
+    hist = stock.history(period=period) #gets the stock history from a given period
+    prices = [] #creates a list that will store the prices over the time period
+
+    #removes the headers of the given history data and isolates the values columns
+    value = (str(hist).replace("                                 Open        High  ...  Dividends  Stock Splits", "\b")).replace("Date                                               ...                         ", "\b").split(" ")
+
+
+    keep = True #creates a boolean that will control what makes it into the value list
+    for i in value: #for loop that goes through the data and isolaetes the value volumns
+        if(isfloat(i) and i != "0.0"):
+            if(keep):
+                prices.append(i)
+            keep = not keep
+
+    return prices #returns a list of prices for the given duration
+
 
 def main():
     portfolio = {}  #map to keep track of transactions
     transaction_id=0  #transation id counter
     net_balance=0  #keeps track of total profits/loss after postions are closed
+    breakdown = [] #stores the portfolio value of each investment for the pie chart
+    percentValue = [] #stores the percentage value of how much each stock uses for pie chart
+    labels = [] #stores the labels of the stocks for the pie chart
+
+    getHistory("MSFT", "1mo")
 
     while True:
         print("\n1. Add Transaction")
@@ -11,9 +44,9 @@ def main():
         print("3. Display Portfolio")
         print("4. Exit")
 
-        choice = input("Enter your choice: ")
+        choice = int(input("Enter your choice: "))
 
-        if choice == '1':
+        if choice == 1:
             ticker_symbol = input("Enter the ticker symbol: ")
 
             qty = int(input("Enter the quantity of stocks bought: "))
@@ -33,7 +66,7 @@ def main():
             transaction_id += 1
 
 
-        elif choice == '2':
+        elif choice == 2:
             transaction_id = int(input("Enter the transaction ID to sell stocks: "))
             if transaction_id in portfolio:
                 sell_price = float(input("Enter the sell price per stock: "))
@@ -44,20 +77,30 @@ def main():
             else:
                 print("Invalid transaction")
 
-        elif choice == '3':
+        elif choice == 3:
             portfolio_value=0
             print("\n--- Portfolio Summary ---")
             print("Ticker Symbol\tQuantity\tBuy Price\tSell Price")
 
             for transaction_id, details in portfolio.items():
                 portfolio_value+=(details[3]-details[2])*(details[1])
+
+                labels.append(details[0]) #stores the labels stored in the map
+                breakdown.append((details[3]-details[2])*(details[1])) #stores the value of each stock
+
                 print(details)
+
+            for i in breakdown: #gets the value of all the stocks in the portfolio
+                percentValue.append(i/portfolio_value) #stores the percentage of the total value each stock has to make a pie chart later on
 
             print("Portfolio Value: $" + str(round(portfolio_value, 2))) #Prints value of portfolio, rounds to 2
             print("Total Profit/Loss: $" + str(round(net_balance, 2))) #Shows net profit/loss, rounds to 2
 
+            y = np.array(percentValue)
+            plt.pie(y, labels=labels, autopct='%1.1f%%') #creates the pie chart with the array data, labels the data with the labels array, and then creates the percentage value
+            plt.show() #prints the pie chart in a separate window
 
-        elif choice == '4':
+        elif choice == 4:
             print("Exiting program.")
             break
 
