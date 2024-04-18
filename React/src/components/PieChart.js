e4ee616276a641d60e82d21a619c19d2f48a0063
anthony madorsky <127152790+admadorsky@users.noreply.github.com>
2024-04-18 04:07:32 -0400

    Delete React/src/components/PieChart.js

--- React/src/components/PieChart.js
+++
@@ -1,8 +0,0 @@
-import 'chart.js/auto'
-import { Doughnut } from "react-chartjs-2"
-
-function PieChart({chartData}) {
-    return <Doughnut data={chartData}></Doughnut>
-}
- 
-export default PieChart;
